a48a5034ff9f0804fa331c1f0eea044a0a62ff0d
ganesh <ganeshgajula22@gmail.com>
2021-07-25 10:31:46 +0530

    refactor: port spell

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the world of Express!!");
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
