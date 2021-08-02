29f3066ff5295758fad14e4c99b8ea9b30b1970a
ganesh <ganeshgajula22@gmail.com>
2021-08-02 23:50:28 +0530

    add: post model

---
+++ models/post.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, required: "Post content cannot be empty" },
+    postDate: String,
+    likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    retweetedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    bookmarkedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    replies: [
+      {
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        content: String,
+        date: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = model("Post", PostSchema);
+
+module.exports = { Post };
