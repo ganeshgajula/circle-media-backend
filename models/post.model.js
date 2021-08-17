d96a5530659c2e18b8f0fedf1ead23fed3d632b8
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-17 23:31:48 +0530

    refactor: post model

--- models/post.model.js
+++ models/post.model.js
@@ -3,22 +3,17 @@ const { Schema, model } = mongoose;
 
 const PostSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: "User" },
-    posts: [
+    userId: { type: Schema.Types.ObjectId, ref: "User", index: true },
+    content: { type: String, required: "Post content cannot be empty" },
+    postDate: String,
+    likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    retweetedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    bookmarkedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    replies: [
       {
-        userId: { type: Schema.Types.ObjectId, ref: "User" },
-        content: { type: String, required: "Post content cannot be empty" },
-        postDate: String,
-        likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        retweetedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        bookmarkedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        replies: [
-          {
-            replierId: { type: Schema.Types.ObjectId, ref: "User" },
-            content: String,
-            date: String,
-          },
-        ],
+        replierId: { type: Schema.Types.ObjectId, ref: "User" },
+        content: String,
+        date: String,
       },
     ],
   },
