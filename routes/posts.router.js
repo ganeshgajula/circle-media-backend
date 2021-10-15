109ebb361844d963009ccb25fdca16c53ace278b
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-10-15 16:50:33 +0530

    add: cloudinary imports

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -3,14 +3,7 @@ const router = express.Router();
 const { extend } = require("lodash");
 const { Post } = require("../models/post.model");
 const { User } = require("../models/user.model");
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-  secure: true,
-});
+const { cloudinary } = require("../utils/cloudinary");
 
 router.param("userId", async (req, res, next, id) => {
   try {
